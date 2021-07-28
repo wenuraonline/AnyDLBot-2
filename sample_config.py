44863e52e970982d8af67373808c7b2ce66a9056
Predator HackerzZ <77227205+PredatorHackerzZ@users.noreply.github.com>
2021-07-29 01:34:04 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -44,3 +44,6 @@ class Config(object):
     parent_id = os.environ.get("GDRIVE_FOLDER_ID","")
     STRIP_FILE_NAMES = os.environ.get("STRIP_FILE_NAMES","www.1TamilMV.life - |www.1TamilMV.life -|www.1TamilMV.org - |www.1TamilMV.org -|www.1TamilMV.xyz - |www.1TamilMV.xyz -|@MoviesFlixers_DL|@TellyFun_Official|@telugu_moviez|@telugupalaka_ht_|@telugupalaka_ht|[MM].|[MM]|[MM] -|@telugu_dubbedmovies_|@telugu_dubbedmovies|@Moviez_India.|@Moviez_India_|@Moviez_India|www_Telugupalaka_com|@MM_New|@MM_Links|@MM_Linkz|www.TamilRockers.ws -|@Animationmovies|HT_BEATS_|-@lubokvideo|@lubokvideo|@english_movieschannel_|@english_movieschannel|@themovies_channel_|@themovies_channel|@telugu_bluray|@TVshows_HD|[Movies Vip]|[CC].|[CC]|@CC_Links.|@CC_Links|@CC_x265.|@CC_x265|@CC.|@CC|@CC_ALL|@CPR_|@CPR|Moviez_India.|Moviez_India")
     CHANNEL_URL = "@TeleRoidGroup\n@TeleRoid14"
+    # Sql Database url
+    DB_URI = os.environ.get("DATABASE_URL", "")
+    
