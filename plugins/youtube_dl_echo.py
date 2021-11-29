6fdcabb6e11ad193c12b01d4284890b50bc4a273
ρяє∂αтσя <77227205+PredatorHackerzZ@users.noreply.github.com>
2021-11-29 21:52:37 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -26,7 +26,6 @@ from translation import Translation
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-from helper_funcs.chat_base import TRChatBase
 from helper_funcs.display_progress import humanbytes
 from helper_funcs.help_uploadbot import DownLoadFile
 
@@ -34,7 +33,6 @@ from helper_funcs.help_uploadbot import DownLoadFile
 @pyrogram.Client.on_message(pyrogram.filters.regex(pattern=".*http.*"))
 async def echo(bot, update):
     # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/echo")
     # await bot.send_chat_action(
     #     chat_id=update.chat.id,
     #     action="typing"
